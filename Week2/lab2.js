033f3f18273fd17cb2fb11bcdc7149a54e17e65b
decimate80 <115748896+decimate80@users.noreply.github.com>
2024-03-05 16:37:45 -0600

    Committing

--- Week2/lab2.js
+++ Week2/lab2.js
@@ -1,22 +1,32 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Software Development' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000,specialization: 'Javascript' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'cryptkeeper' },
     //... More employee records can be added here
   ];
-   // Function to display all employees
-const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
-document.getElementById('employeesDetails').innerHTML = totalEmployees;
-
-function calculateTotalSalaries() {
-    const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
-    alert(`Total Salaries: $${totalSalaries}`);
-  }
-  function displayHREmployees() {
+function findBySpecialization() {
+    const JavascriptEmployees = employees.filter(employee => employee.specialization === 'Javascript');
+     const JavascriptEmployeesDisplay = JavascriptEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+     document.getElementById('employeesDetails').innerHTML = JavascriptEmployeesDisplay;
+}
+function displayHREmployees() {
     const hrEmployees = employees.filter(employee => employee.department === 'HR');
      const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
      document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
 }
+
+
+
+
+function displayEmployees() {
+const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
+function calculateTotalSalaries() {
+    const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+    alert(`Total Salaries: $${totalSalaries}`);
+  }
+
 function findEmployeeById(employeeId) {
     const foundEmployee = employees.find(employee => employee.id === employeeId);
     if (foundEmployee) {
