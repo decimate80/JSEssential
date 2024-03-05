0016bf48aa883992824db0c6439df9ed60466174
decimate80 <115748896+decimate80@users.noreply.github.com>
2024-03-05 13:43:58 -0600

    Totally committed

--- Week2/lab1.js
+++ Week2/lab1.js
@@ -1,9 +1,11 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal-$21.99', 'Frittata-$15'];
+const mainCourseMenu = ['Steak - $29.99', 'Pasta- $19.99', 'Burger- $14.99', 'Salmon- $22.99'];
+const dessertMenu = ['Cake- $10', 'Ice Cream- $8', 'Pudding- $12', 'Fruit Salad- $15', 'One Dorito- $1'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+
 
 let mainCourseItem = '';
 mainCourseMenu.forEach((item, index) => {
@@ -14,3 +16,4 @@ let dessertItem = '';
 for (let i = 0; i < dessertMenu.length; i++) {
     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
 document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+
